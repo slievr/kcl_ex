80132bad82fd7603e0bdc691848cf91aa7cc016f
slievr <pacpellet@gmail.com>
2021-05-19 20:03:00 +0100

    log starting unique stream.

--- lib/kinesis_client/stream.ex
+++ lib/kinesis_client/stream.ex
@@ -104,6 +104,11 @@ defmodule KinesisClient.Stream do
   defp get_shard_supervisor(opts) do
     name = Module.concat(KinesisClient.Stream.ShardSupervisor, opts[:stream_name])
     supervisor_name = Module.concat(name,Integer.to_string(:erlang.unique_integer()))
+
+    Logger.debug(
+      "Starting KinesisClient.Stream: [name: #{supervisor_name}, stream_name: #{name}]"
+    )
+
     {{DynamicSupervisor, [strategy: :one_for_one, name: supervisor_name]}, name}
   end
 
